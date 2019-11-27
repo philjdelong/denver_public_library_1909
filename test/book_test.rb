53b5e7897dfee6f48a88e82e29f2d0ed599200a9
Phil DeLong <philjdelong@gmail.com>
2019-11-27 13:44:16 -0700

    Complete book test and setup initialize

---
+++ test/book_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/book'
+
+class BookTest < Minitest::Test
+  def setup
+    @book = Book.new({  author_first_name: "Harper",
+                        author_last_name: "Lee",
+                        title: "To Kill a Mockingbird",
+                        publication_date: "July 11, 1960"
+                      })
+  end
+
+  def test_it_exists
+    assert_instance_of Book, @book
+  end
+
+  def test_it_initializes_with_4_attributes
+    assert_equal "Harper", @book.author_first_name
+    assert_equal "Lee", @book.author_last_name
+    assert_equal "To Kill a Mockingbird", @book.title
+    assert_equal "July 11, 1960", @book.publication_date
+  end
+end
